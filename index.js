bc2cd3ba4e2afefee3f8551e61127dbe7081a5ae
Jakeer <sjakeerhussain244@gmail.com>
2024-08-22 21:22:37 +0530

    submit 3

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+
 const cors=require("cors")
 const app = express();
 const PORT = 3010;
 
-// Connect to the SQLite database
-const db = new sqlite3.Database('./database.sqlite');
-
-// Middleware to parse JSON requests
-app.use(express.json());
 app.use(cors())
 
-// Import and use routes
-const mentorRoutes = require('./routes/mentors');
-const bookingRoutes = require('./routes/bookings');
-const studentRoutes = require('./routes/students');
-
-app.use('/mentors', mentorRoutes(db));
-app.use('/bookings', bookingRoutes(db));
-app.use('/students', studentRoutes(db));
 app.use("/",(req,res)=>{
     res.send(`<h1>Home Page Node</h1>`)
 })
