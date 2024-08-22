d428d8dd689ab84a72af1d7a5c4d959529b55755
Shaik Jakeer Hussain <127141771+Jvjjggj@users.noreply.github.com>
2024-08-22 23:46:20 +0530

    Update mentors.js

--- routes/mentors.js
+++ routes/mentors.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMentors } = require('../controllers/mentorsController');
 
 module.exports = (db) => {
-    router.get('/', getAllMentors(db));
+    router.get('/', (req, res, next) => {
+        db.all('SELECT * FROM mentors', (err, rows) => {
+            if (err) {
+                return next(err); // Pass errors to the error-handling middleware
+            }
+            res.json(rows);
+        });
+    });
+
+    router.post('/', (req, res, next) => {
+        // Example POST request handling
+        // Implement your logic here
+        res.send('POST request to /mentors');
+    });
+
     return router;
 };
